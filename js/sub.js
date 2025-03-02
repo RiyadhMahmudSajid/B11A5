7ef908beacccc6cc3e4c8b2acf8236725e7a6283
RiyadhMahmudSajid <rdhsajid@gmail.com>
2025-03-02 10:45:54 +0600

    error-solve

--- js/sub.js
+++ js/sub.js
@@ -32,12 +32,6 @@ for(let i = 0; i<commonBtnn.length ; i++){
 
    
 }
-// const rtr = document.querySelectorAll('.text-t')
-// for(let i = 0; i< rtr.length;i++){
-//     console.log(rtr[i])
-// }
-
-
 
 
 function myfun(){
@@ -71,7 +65,7 @@ function myfun1(){
     const contain = document.getElementById('aside-container')
     const p = document.createElement("p");
      p.classList.add("h-16", "p-6","bg-[#F4F7FF]","mt-6","rounded-2xl", "flex","items-center")
-    p.innerText = `${"You have complted the task "}${document.querySelector('.text-t ').innerText}${timeri}`
+    p.innerText = `${"You have complted the task "}${document.querySelector('.fir-h2  ').innerText}${timeri}`
     contain.appendChild(p)
 
 }
@@ -88,12 +82,10 @@ function myfun2(){
     let timeri = ` ${hour}:${minutes}:${seconds} ${prepand }`
     const contain = document.getElementById('aside-container')
     const p = document.createElement("p");
-    p.classList.add("h-16", "p-6","bg-[#F4F7FF]","mt-6","rounded-2xl", "flex","items-center")
-    p.innerText = `${"You have complted the task "}${document.querySelector('.sec-h2').innerText}${timeri}`
+     p.classList.add("h-16", "p-6","bg-[#F4F7FF]","mt-6","rounded-2xl", "flex","items-center")
+    p.innerText = `${"You have complted the task "}${document.querySelector('.sec-h2 ').innerText}${timeri}`
     contain.appendChild(p)
-
-    // p.innerText = `${"You have complted the task "}${document.querySelector('.sec-h2').innerText}`
-    // contain.appendChild(p)
+   
 }
 function myfun3(){
     const currentDate = new Date();
